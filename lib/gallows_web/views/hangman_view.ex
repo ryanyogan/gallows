7cc3afe033ab2c186aa0820682cf760e9701a6bd
ryanyogan <ryanyogan@gmail.com>
2020-05-10 16:46:57 -0700

    can play a basic (ssr) version of hangman :-)

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,7 +1,18 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+  import Gallows.Views.Helpers.GameStateHelper
 
-  def word_so_far(letters) when is_list(letters) do
-    letters |> Enum.join(" ")
+  def game_over?(%{game_state: game_state}) do
+    game_state in [:won, :lost]
   end
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(_left, _target), do: "opacity: 0.1"
 end
---
+++ lib/gallows_web/views/helpers/game_state_helper.ex
@@ -0,0 +1,31 @@
+defmodule Gallows.Views.Helpers.GameStateHelper do
+  import Phoenix.HTML, only: [raw: 1]
+
+  @responses %{
+    :won => {:success, "You Won!"},
+    :lost => {:danger, "You Lost!"},
+    :good_guess => {:success, "Good guess!"},
+    :bad_guess => {:warning, "Bad guess!"},
+    :already_used => {:info, "You already guessed that!"}
+  }
+
+  def word_so_far(letters) when is_list(letters) do
+    letters |> Enum.join(" ")
+  end
+
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  defp alert(nil), do: ""
+
+  defp alert({class, message}) do
+    """
+    <div class="alert alert-#{class}">
+      #{message}
+    </div>
+    """
+    |> raw()
+  end
+end
